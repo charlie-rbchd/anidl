261691d71d395e3876bc70ceaa43b2d1340b9ecc
Joel Robichaud <robichaud.jo@gmail.com>
2015-03-13 20:05:55 -0400

    Added support for Mac OS X app bundling using py2app.

--- README.md
+++ README.md
@@ -3,12 +3,19 @@
 Before you can use the project, you must first install mechanize, beautifulsoup4, html5lib & [wxPython](http://www.wxpython.org/). You must also have a valid [anilist API client identifier and secret key](http://anilist-api.readthedocs.org/en/latest/introduction.html#creating-a-client). Once you have that, you need to set the ```ANILIST_CLIENT_ID``` and ```ANILIST_CLIENT_SECRET``` constants to these values in the ```config.py``` file.
 
 ## Compiling the project ##
-Before you can compile the project, you must first install [py2exe](http://www.py2exe.org/). You must also make sure you have the ```msvcp90.dll``` file __version 9.0.21022.8__ placed in your python DLLs folder.
+Before you can compile the project, you must first install either [py2exe](http://www.py2exe.org/) or [py2app](https://pythonhosted.org/py2app/) depending on your OS. If you are on Windows, you must also make sure you have the ```msvcp90.dll``` file __version 9.0.21022.8__ placed in your python DLLs folder.
 
-Then, in a command prompt, enter the following commands:
+Then, in a command prompt, enter the following commands.
+For Windows:
 ```
 cd /path/to/project/
 python setup.py py2exe
+```
+
+For Mac OS X:
+```
+cd /path/to/project/
+python setup.py p2app
 ```
 
 The resulting executable will be placed under the ```dist``` folder.
--- setup.py
+++ setup.py
@@ -1,32 +1,61 @@
+import sys
 from distutils.core import setup
-import py2exe
 
-includes = []
-excludes = ["_gtkagg", "_tkagg", "bsddb", "curses", "email", "pywin.debugger",
-            "pywin.debugger.dbgcon", "pywin.dialogs", "tcl", "Tkconstants", "Tkinter"]
-packages = []
-dll_excludes = ["libgdk-win32-2.0-0.dll", "libgobject-2.0-0.dll", "tcl84.dll", "tk84.dll", "w9xpopen.exe"]
+if sys.platform == "darwin":
+    import py2app
 
-for dbmodule in ["dbhash", "gdbm", "dbm", "dumbdbm"]:
-    try:
-        __import__(dbmodule)
-    except ImportError:
-        pass
-    else:
-         # If we found the module, ensure it"s copied to the build directory.
-        packages.append(dbmodule)
+    dylib_excludes = []
+    options = {"py2app": {"optimize": 2,
+                          "dist_dir": "dist",
+                          "xref": False,
+                          "strip": True,
+                          "argv_emulation": True,
+                          "site_packages": True,
+                          "arch": "i386",
+                          "iconfile": "anidl.icns",
+                          "resources": ["anidl.ico"],
+                          "plist": {"CFBundleName": "anidl",
+                                    "CFBundleShortVersionString":"1.0.0",
+                                    "CFBundleVersion": "1.0.0",
+                                    "CFBundleDevelopmentRegion": "English"}}}
 
-setup(windows = [{"script": "anidl.py", "icon_resources": [(1, "anidl.ico")]}],
-      zipfile = None,
-      options = {"py2exe": {"compressed": 2,
-                            "optimize": 2,
-                            "includes": includes,
-                            "excludes": excludes,
-                            "packages": packages,
-                            "dll_excludes": dll_excludes,
-                            "bundle_files": 1,
-                            "dist_dir": "dist",
-                            "xref": False,
-                            "skip_archive": False,
-                            "ascii": False,
-                            "custom_boot_script": ""}})
+    args = {"app": ["anidl.py"], "options": options}
+elif sys.platform == "win32":
+    import py2exe
+
+    includes = []
+    excludes = ["_gtkagg", "_tkagg", "bsddb", "curses", "email", "pywin.debugger",
+                "pywin.debugger.dbgcon", "pywin.dialogs", "tcl", "Tkconstants", "Tkinter"]
+
+    packages = []
+    for dbmodule in ["dbhash", "gdbm", "dbm", "dumbdbm"]:
+        try:
+            __import__(dbmodule)
+        except ImportError:
+            pass
+        else:
+             # If we found the module, ensure it"s copied to the build directory.
+            packages.append(dbmodule)
+
+    dll_excludes = ["libgdk-win32-2.0-0.dll", "libgobject-2.0-0.dll", "tcl84.dll", "tk84.dll", "w9xpopen.exe"]
+
+    options = {"py2exe": {"compressed": 2,
+                          "optimize": 2,
+                          "includes": includes,
+                          "excludes": excludes,
+                          "packages": packages,
+                          "dll_excludes": dll_excludes,
+                          "bundle_files": 1,
+                          "dist_dir": "dist",
+                          "xref": False,
+                          "skip_archive": False,
+                          "ascii": False,
+                          "custom_boot_script": ""}}
+
+    args = {"windows": [{"script": "anidl.py", "icon_resources": [(1, "anidl.ico")]}],
+            "zipfile": None,
+            "options": options}
+else:
+    args = {"scripts": ["anidl.py"]}
+
+setup(name="anidl", **args)
